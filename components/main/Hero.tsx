88c090afd814d8b24a58c1b86cd95304b8f47b5b
ADITYA-BHATTACHARYA-DEV <161118055+ADITYA-BHATTACHARYA-DEV@users.noreply.github.com>
2025-01-31 14:52:47 +0530

    Initial Codebase

---
+++ components/main/Hero.tsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+    return (
+        <div className="relative flex flex-col h-full w-full">
+            <video 
+                autoPlay
+                muted
+                loop
+                className="relative-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover"
+            >
+                <source src="/blackhole.webm" type="video/webm" />
+                
+            </video>
+        </div>
+    );
+};
+
+export default Hero;

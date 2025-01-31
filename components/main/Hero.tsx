1efa71415836917ad8fc68d3ecba38cf55af1b5c
ADITYA-BHATTACHARYA-DEV <161118055+ADITYA-BHATTACHARYA-DEV@users.noreply.github.com>
2025-01-31 15:42:56 +0530

    Updated

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,3 +1,5 @@
+import HeroContent from "../sub/HeroContent";
+
 const Hero = () => {
     return (
         <div className="relative flex flex-col h-full w-full">
@@ -8,8 +10,8 @@ const Hero = () => {
                 className="relative-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover"
             >
                 <source src="/blackhole.webm" type="video/webm" />
-                
             </video>
+            <HeroContent />
         </div>
     );
 };
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,7 +1,10 @@
 
-import { slideInFromTop } from '@/utils/motion'
-import { motion } from 'framer-motion'
+"use client";
 
+import { slideInFromLeft, slideInFromTop } from '@/utils/motion';
+import { motion } from 'framer-motion';
+// import { SparkleIcon, SparklesIcon } from 'lucide-react'
+import { SparklesIcon } from '@heroicons/react/24/solid';
 const HeroContent = () =>
 {
     return (
@@ -14,10 +17,22 @@ const HeroContent = () =>
             <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
                 <motion.div
                 variants={slideInFromTop}
-                className='Welcome-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9]'>
+                className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'>
+
+                        <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
+                        <h1 className='Welcome-text text-[13px]'>Software Developer Portfolio</h1>
+
+                </motion.div>
+                <motion.div
+                variants={slideInFromLeft(0.5)}
+                className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+                >
+
 
                 </motion.div>
             </div>
         </motion.div>
     )
 }
+
+export default HeroContent
